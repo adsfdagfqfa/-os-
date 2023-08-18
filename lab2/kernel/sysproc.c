49f7e72ad8e96d824e4d8775390aafcb3b4aeb1f
xue <134774045+adsfdagfqfa@users.noreply.github.com>
2023-08-18 10:09:57 +0800

    Add files via upload

---
+++ lab2/kernel/sysproc.c
@@ -0,0 +1,124 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int mask;
+  argint(0, &mask); //从内核中获取参数int参数
+
+  myproc()->mask = mask; //对进程的掩码进行设置
+
+  return 0;
+}
+#include "sysinfo.h"
+extern uint64 getfreemem(void);
+extern uint64 getnproc(void);
+
+uint64 
+sys_sysinfo(void){
+  struct sysinfo info;//储存内容的结构体
+  uint64 addr;//首先从寄存器a0中获取参数指针
+  argaddr(0,&addr);
+  info.freemem=getfreemem();
+  info.nproc=getnproc();
+  if(copyout(myproc()->pagetable, addr, (char *)&info, sizeof(info)) < 0)//copyout函数将info拷贝到用户态进程中。
+      return -1;
+
+  return 0;
+}
